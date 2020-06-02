0439b5cf4c0d20f83466cecc23faf06f611d28f9
Aleksey Belchenkov <u608110@gmail.com>
2020-06-02 18:13:47 +0300

    Adding a role to the user model

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -33,7 +33,8 @@ const signin = async (req, res) => {
             user: {
                 _id: user._id,
                 name: user.name,
-                email: user.email
+                email: user.email,
+                educator: user.educator
             }
         });
     } catch (err) {
